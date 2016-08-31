25545c758d6cad3cc8bc466dd488cc6af31a77e2
Konstantin Vorobyev <vorobyev@coacha.com>
2016-08-31 11:41:31 +0900

    Automate binary packaging and upload

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+version=`node -e "const p = require('./package.json'); console.log(p.version);"`
 platform=${1-"linux"}
-version=${2-"SNAPSHOT"}
+arch=${2-"all"}
 
 rm -rf out/ .desktop/
 npm run dist
 cp desktop.js index.js
-node_modules/.bin/electron-packager ./ --platform=$platform --arch=all \
+node_modules/.bin/electron-packager ./ --platform=$platform --arch=$arch \
 	--out=out/ --prune --asar \
 	--icon=images/app_icon \
+	--app-version=$version \
 	--ignore="android($|/)" \
 	--ignore="ios($|/)" \
 	--ignore="app($|/)" \
@@ -31,6 +33,8 @@ do
 	dir=${dir%*/}
 	echo $dir
 	zip -r ${dir}-$version.zip $dir
+	echo "Uploading to Bitbucket..."
+	curl -s -u kvorobyev -X POST https://api.bitbucket.org/2.0/repositories/kvorobyev/taskwarriorc2/downloads -F files=@${dir}-$version.zip
 done
 cd ..
 
